0c87a153f65ee2e9aee6f0dc30b7c889d1584197
austencm <austencm@gmail.com>
2026-05-07 23:18:16 -0700

    feat(ghostrunner): filesystem helpers + AppData path discovery

--- GhostRunner/scripts/mods/GhostRunner/GhostRunner.lua
+++ GhostRunner/scripts/mods/GhostRunner/GhostRunner.lua
@@ -11,4 +11,15 @@ end
 
 mod.SoloPlay = SoloPlay
 
+mod.fs = mod:io_dofile("GhostRunner/scripts/mods/GhostRunner/fs")
+mod.fs.ensure_runs_folder()
+mod:info("GhostRunner runs folder: " .. tostring(mod.fs.runs_root))
+
+mod:command("ghost_test_fs", "GhostRunner: verify filesystem helpers", function()
+	mod:info("[fs] runs_root: " .. tostring(mod.fs.runs_root))
+	mod:info("[fs] runs_path('foo.run'): " .. tostring(mod.fs.runs_path("foo.run")))
+	mod:info("[fs] index_path: " .. tostring(mod.fs.index_path()))
+	mod:info("[fs] list_run_files: count=" .. tostring(#mod.fs.list_run_files()))
+end)
+
 return mod
